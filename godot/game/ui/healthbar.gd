c3abc20a4bd759f5f2c7acc0dc31c41e466010c4
Sophia Köhler <sophia.koehler@staffbase.com>
2023-04-29 22:06:48 +0200

    Correct .gitignore and format file.

--- godot/game/ui/healthbar.gd
+++ godot/game/ui/healthbar.gd
@@ -2,12 +2,15 @@ extends Control
 
 @onready var health_bar = $Healthbar
 
+
 func _on_health_updated(health, amount) -> void:
 	health_bar.value = health
+
 
 func _tween(value) -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "value", value, 1.0).set_trans(Tween.TRANS_LINEAR)
+
 
 # for testing purposes
 func _ready():
