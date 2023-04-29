1db7eee4516084dccc789329a8464b93c2703cd1
Sophia Köhler <sophia.koehler@staffbase.com>
2023-04-29 19:06:49 +0200

    Apply formating.

--- godot/game/balloon/balloon.gd
+++ godot/game/balloon/balloon.gd
@@ -6,6 +6,7 @@ const MAX_WIND_SPEED = 300.0
 const MAX_RAW_WIND_POWER = 400.0
 const WIND_POWER_DROP = 1.0
 
+
 func get_wind_speed():
 	var wind_speed = Vector2(0, 0)
 	if Input.is_action_pressed("blow_wind"):
@@ -14,36 +15,42 @@ func get_wind_speed():
 		wind_speed = wind_direction * MAX_WIND_SPEED * wind_power
 	return wind_speed
 
+
 func get_wind_direction():
 	var wind_origin = get_global_mouse_position()
-	if (position.is_equal_approx(wind_origin)):
+	if position.is_equal_approx(wind_origin):
 		return Vector2(0, 0)
 	var wind_direction = -position.direction_to(wind_origin)
 	return wind_direction
 
+
 func get_wind_power():
 	var wind_origin = get_global_mouse_position()
 	var distance = position.distance_to(wind_origin)
-	var wind_raw_power = pow(1/(distance), WIND_POWER_DROP)
-	return max(wind_raw_power/ MAX_RAW_WIND_POWER, 1)
+	var wind_raw_power = pow(1 / (distance), WIND_POWER_DROP)
+	return max(wind_raw_power / MAX_RAW_WIND_POWER, 1)
+
 
 func adjust_rotation():
 	rotation = asin(velocity.x / MAX_WIND_SPEED)
 
+
 func adjust_velocity(wind_speed):
-	velocity += (wind_speed - velocity )* DRAG
+	velocity += (wind_speed - velocity) * DRAG
+
 
 func reset_outside_position():
-	if (position.x < 50):
+	if position.x < 50:
 		position.x = 50
 	move_and_slide()
-	if (position.y < 50):
+	if position.y < 50:
 		position.y = 50
-	if (position.x > 1800):
+	if position.x > 1800:
 		position.x = 1800
 	move_and_slide()
-	if (position.y > 1000):
+	if position.y > 1000:
 		position.y = 1000
+
 
 func _physics_process(_delta):
 	var wind_speed = get_wind_speed()
@@ -51,4 +58,3 @@ func _physics_process(_delta):
 	adjust_velocity(wind_speed)
 	move_and_slide()
 	reset_outside_position()
-	
