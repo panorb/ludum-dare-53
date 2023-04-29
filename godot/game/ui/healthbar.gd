f8f958c015f6d43d7da52d016ddb6b2fd7a10925
Sophia Köhler <sophia.koehler@staffbase.com>
2023-04-29 22:36:56 +0200

    Change color of bar.

--- godot/game/ui/healthbar.gd
+++ godot/game/ui/healthbar.gd
@@ -1,9 +1,27 @@
 extends Control
 
+@export var healthy_color: Color = Color.DARK_GREEN
+@export var warning_color: Color = Color.ORANGE
+@export var danger_color: Color = Color.RED
+
+@export var warning_amount: float = 0.5
+@export var danger_amount: float = 0.25
+
 
 func _on_health_updated(amount) -> void:
 	var health = self.value - amount
+
 	_tween(health)
+	_assign_color(health)
+
+
+func _assign_color(amount) -> void:
+	if amount < self.max_value * danger_amount:
+		self.tint_progress = danger_color
+	elif amount < self.max_value * warning_amount:
+		self.tint_progress = warning_color
+	else:
+		self.tint_progress = healthy_color
 
 
 func _tween(value) -> void:
@@ -13,4 +31,4 @@ func _tween(value) -> void:
 
 # for testing purposes
 func _ready():
-	_on_health_updated(20)
+	_on_health_updated(15)
