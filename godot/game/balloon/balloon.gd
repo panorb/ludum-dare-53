fa3cb3433f7e5199128804f0b043b2ba8763de57
JulianPL <j.pape-lange@gmx.de>
2023-04-29 19:35:18 +0200

    Apply suggestions from code review
    Co-authored-by: Sophia Köhler <55245289+xsoophx@users.noreply.github.com>

--- godot/game/balloon/balloon.gd
+++ godot/game/balloon/balloon.gd
@@ -8,20 +8,18 @@ const WIND_POWER_DROP = 1.0
 
 
 func get_wind_speed():
-	var wind_speed = Vector2(0, 0)
 	if Input.is_action_pressed("blow_wind"):
 		var wind_direction = get_wind_direction()
 		var wind_power = get_wind_power()
-		wind_speed = wind_direction * MAX_WIND_SPEED * wind_power
-	return wind_speed
+		return wind_direction * MAX_WIND_SPEED * wind_power
+	return Vector2(0, 0)
 
 
 func get_wind_direction():
 	var wind_origin = get_global_mouse_position()
 	if position.is_equal_approx(wind_origin):
 		return Vector2(0, 0)
-	var wind_direction = -position.direction_to(wind_origin)
-	return wind_direction
+	return -position.direction_to(wind_origin)
 
 
 func get_wind_power():
