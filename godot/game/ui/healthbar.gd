0a143d5125f7f8911c95f9861415351872830489
Sophia Köhler <sophia.koehler@staffbase.com>
2023-04-29 21:58:34 +0200

    Add healthbar-ish thing.

---
+++ godot/game/ui/healthbar.gd
@@ -0,0 +1,14 @@
+extends Control
+
+@onready var health_bar = $Healthbar
+
+func _on_health_updated(health, amount) -> void:
+	health_bar.value = health
+
+func _tween(value) -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "value", value, 1.0).set_trans(Tween.TRANS_LINEAR)
+
+# for testing purposes
+func _ready():
+	_tween(100)
