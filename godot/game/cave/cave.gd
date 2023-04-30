a9039aa245131b6c57a05cffbc233ac3aef92f6b
Julian Pape-Lange <j.pape-lange@gmx.de>
2023-04-30 09:52:00 +0200

    Improve performance and make constants for cave segments more stable

--- godot/game/cave/cave.gd
+++ godot/game/cave/cave.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const SPEED = 10
+const SPEED = 5
 
 const SCREEN_HEIGHT = 1080
 
@@ -10,11 +10,11 @@ var starting_position = Vector2(0,0)
 var walls = []
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	for i in 3:
 		new_cave_segment()
 
-func new_cave_segment():
+func new_cave_segment() -> void:
 	var instance = segment.instantiate()
 	add_child(instance)
 	if walls.is_empty():
@@ -24,13 +24,16 @@ func new_cave_segment():
 	starting_position = instance.create_cave_walls(starting_position)
 	walls.append(instance)
 
+func redraw_cave_segment() -> void:
+	walls[0].position.y = walls.back().position.y - SCREEN_HEIGHT
+	starting_position = walls[0].create_cave_walls(starting_position)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta) -> void:
 	walls[0].position.y += SPEED
 	walls[1].position.y += SPEED
 	walls[2].position.y += SPEED
 	
 	if walls[1].position.y > 0:
-		walls[0].remove_child(walls[0].get_child(0))
-		walls.pop_front()
-		new_cave_segment()
+		redraw_cave_segment()
+		walls.append(walls.pop_front())
--- godot/game/cave_segment/cave_segment.gd
+++ godot/game/cave_segment/cave_segment.gd
@@ -3,12 +3,13 @@ extends CharacterBody2D
 const SCREEN_HEIGHT = 1080
 const SCREEN_WIDTH = 1920
 
-const SAMPLES = 10
+const SAMPLES = 8
 
-var EFFECTIVE_WIDTH = 1020
-var MIN_BORDER = 100
-var SYMMETRIC_VARIANCE = 200
+var EFFECTIVE_WIDTH = 1300
+var MIN_BORDER = 1
+var SYMMETRIC_VARIANCE = 100
 var ASYMMETRIC_VARIANCE = 300
+var HORIZONTAL_VARIANCE = 40
 
 @onready var segment_left = get_node("SegmentLeft")
 @onready var collision_segment_left = get_node("CollisionSegmentLeft")
@@ -18,7 +19,7 @@ var ASYMMETRIC_VARIANCE = 300
 func _ready() -> void:
 	pass
 
-func create_cave_walls(starting_position):
+func create_cave_walls(starting_position) -> Vector2:
 	var left_column = [Vector2(0, 0), Vector2(0, SCREEN_HEIGHT), Vector2(starting_position[0], SCREEN_HEIGHT)]
 	var right_column = [Vector2(SCREEN_WIDTH, 0), Vector2(SCREEN_WIDTH, SCREEN_HEIGHT), Vector2(starting_position[1], SCREEN_HEIGHT)]
 	var left_offset_bottom = randi_range(MIN_BORDER, SCREEN_WIDTH - EFFECTIVE_WIDTH - MIN_BORDER)
@@ -32,10 +33,16 @@ func create_cave_walls(starting_position):
 		left_position += randi_range(-ASYMMETRIC_VARIANCE/2, ASYMMETRIC_VARIANCE/2)
 		right_position += randi_range(-ASYMMETRIC_VARIANCE/2, ASYMMETRIC_VARIANCE/2) 
 		left_position = max( 1, left_position)
-		right_position = min( SCREEN_WIDTH, right_position)
+		right_position = min( SCREEN_WIDTH - 1, right_position)
 		var vertical_position = SCREEN_HEIGHT - (n + 1) * SCREEN_HEIGHT / SAMPLES
-		left_column.append(Vector2(left_position, vertical_position))
-		right_column.append(Vector2(right_position, vertical_position))
+		if n == SAMPLES - 1:
+			left_column.append(Vector2(left_position, vertical_position))
+			right_column.append(Vector2(right_position, vertical_position))
+		else:
+			var left_vertical_position = vertical_position + randi_range(-HORIZONTAL_VARIANCE/2, HORIZONTAL_VARIANCE/2)
+			var right_vertical_position = vertical_position + randi_range(-HORIZONTAL_VARIANCE/2, HORIZONTAL_VARIANCE/2)
+			left_column.append(Vector2(left_position, left_vertical_position))
+			right_column.append(Vector2(right_position, right_vertical_position))
 	segment_left.polygon = PackedVector2Array(left_column)
 	collision_segment_left.polygon = PackedVector2Array(left_column)
 	segment_right.polygon = PackedVector2Array(right_column)
