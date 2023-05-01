6c704a98a061ed8c27d84a35b7110fac7d4603a1
Julian Pape-Lange <j.pape-lange@gmx.de>
2023-05-02 01:03:48 +0200

    Add tree obstacles

--- godot/game/cave/cave.gd
+++ godot/game/cave/cave.gd
@@ -4,6 +4,8 @@ var globals = preload("res://autoloads/globals.gd")
 
 var segment = load("res://game/cave_segment/cave_segment.tscn")
 var bat = load("res://game/cave/obstacles/bat/bat.tscn")
+var tree1 = load("res://game/cave/obstacles/tree1/tree1.tscn")
+var tree2 = load("res://game/cave/obstacles/tree2/tree2.tscn")
 
 @onready var background_audio = $BackgroundAudioStreamPlayer
 
@@ -11,6 +13,7 @@ var starting_position = Vector2(globals.SCREEN_WIDTH / 2 - globals.GUARANTEED_CA
 var iterations = 0
 var walls = []
 var bats = []
+var trees = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,6 +23,17 @@ func _ready() -> void:
 		for bat_spawn_position in wall.bat_spawn_positions:
 			if 1 >= randf_range(0, globals.BAT_RARENESS):
 				create_bat(bat_spawn_position + wall.position)
+		for tree_spawn_position in wall.tree_spawn_positions:
+			if 1 >= randf_range(0, globals.TREE_RARENESS):
+				var tree_x = (tree_spawn_position[0] + tree_spawn_position[1]) / 2 + wall.position[0]
+				var tree_y = tree_spawn_position[2] + wall.position[1]
+				create_tree(Vector2(tree_x, tree_y))
+				if randi() % 2 == 0:
+					tree_x += globals.TREE_SPAWN_MIN_DISTANCE
+				else:
+					tree_x -= globals.TREE_SPAWN_MIN_DISTANCE
+				if 1 >= randf_range(0, globals.TREE_RARENESS):
+					create_tree(Vector2(tree_x, tree_y))
 	start_background_music()
 
 func new_cave_segment() -> void:
@@ -44,13 +58,26 @@ func create_bat(bat_spawn_position) -> void:
 	instance.position = bat_spawn_position
 	bats.append(instance)
 
+func create_tree(tree_spawn_position) -> void:
+	var instance
+	if randi() % 2 == 0:
+		instance = tree1.instantiate()
+	else:
+		instance = tree2.instantiate()
+	add_child(instance)
+	instance.position = tree_spawn_position
+	trees.append(instance)
+
 func reset() -> void:
 	for wall in walls:
 		wall.queue_free()
 	for bat in bats:
 		bat.queue_free()
+	for tree in trees:
+		tree.queue_free()
 	walls = []
 	bats = []
+	trees = []
 	_ready()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
